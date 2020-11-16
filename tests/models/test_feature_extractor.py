85f1c4e9a07e91fa9198e26b0d24fc0bd91115d1
Emmanuel Bello <emabello42@gmail.com>
2020-11-16 11:45:38 +0100

    TEST: feature extractor

---
+++ tests/models/test_feature_extractor.py
@@ -0,0 +1,17 @@
+from visualsearch.models import feature_extractor as fe
+import os
+import pytest
+import numpy as np
+
+FIXTURE_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../testdata")
+
+@pytest.mark.datafiles(os.path.join(FIXTURE_DIR, 'img1.jpg'))
+def test_process_image(datafiles):
+    path = str(datafiles)
+    feature_extractor = fe.FeatureExtractor()
+    input_image = os.path.join(path, "img1.jpg")
+    features = feature_extractor.process_image(path = input_image)
+    assert np.linalg.norm(features['unit_features']) == pytest.approx(1.0)
+    assert features['magnitude'] >= 0
+    assert features['label'] >= 0
+    assert 0.0 <= features['score'] <= 1.0
