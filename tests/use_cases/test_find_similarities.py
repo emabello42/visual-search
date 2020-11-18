d090245d2ec920ee055d202c3917a5887c0d1b31
Emmanuel Bello <emabello42@gmail.com>
2020-11-18 22:23:26 +0100

    test: redo test for find similarities use case

--- tests/use_cases/test_find_similarities.py
+++ tests/use_cases/test_find_similarities.py
@@ -1,4 +1,5 @@
 import os
+import logging
 import pytest
 from unittest import mock
 from visualsearch.domain import image as i
@@ -6,6 +7,8 @@ from visualsearch.use_cases import find_similarities as uc
 import visualsearch.request_objects as req
 import uuid
 import numpy as np
+from visualsearch.models.feature_extractor import ImageFeatures
+
 FIXTURE_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../testdata")
 
 @pytest.fixture
@@ -20,21 +23,40 @@ def domain_images():
     return [img1, img2]
 
 
-@pytest.mark.datafiles(
-        os.path.join(FIXTURE_DIR, 'img1.jpg'),
-        os.path.join(FIXTURE_DIR, 'img2.jpg'),
-        os.path.join(FIXTURE_DIR, 'img3.jpg'),
-        )
-def test_find_similarities_with_path(datafiles, domain_images):
+@pytest.fixture
+def img_features_list():
+    img_feat1 = ImageFeatures(
+                            unit_features = np.arange(2048),
+                            magnitude = 101.1,
+                            path = os.path.join(FIXTURE_DIR, 'img1.jpg')
+                            )
+    img_feat2 = ImageFeatures(
+                            unit_features = np.arange(2048),
+                            magnitude = 102.1,
+                            path = os.path.join(FIXTURE_DIR, 'img2.jpg')
+                            )
+
+    img_feat3 = ImageFeatures(
+                            unit_features = np.arange(2048),
+                            magnitude = 103.1,
+                            path = os.path.join(FIXTURE_DIR, 'img3.jpg')
+                            )
+
+    return [img_feat1, img_feat2, img_feat3]
+
+
+def test_find_similarities_with_path(img_features_list, domain_images):
     # create feature_extractor and repo mocks
-    path = str(datafiles)  # Convert from py.path object to path (str)
     repo = mock.Mock()
+    feature_extractor = mock.Mock()
     repo.find_similars.return_value = domain_images
-    
-    input_image = os.path.join(path, "img1.jpg")
-    request_object = req.FindSimilaritiesRequestObject.from_dict({'params':{'path': input_image}})
-    fs_use_case = uc.FindSimilarities(repository = repo)
+    feature_extractor.process_image.return_value = img_features_list[2]
+    file_path = img_features_list[2].path
+    request_object = req.FindSimilaritiesRequestObject.from_dict({'params':{'path': file_path}})
+    fs_use_case = uc.FindSimilarities(repo, feature_extractor)
     response = fs_use_case.execute(request_object)
+    logging.debug(response.value)
     assert bool(response) is True
-    repo.find_similars.assert_called_with(file_path = input_image)
+    feature_extractor.process_image.assert_called_with(file_path)
+    repo.find_similars.assert_called()
     assert response.value == domain_images
