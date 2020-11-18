5f6cf24acd22927acebd3777db37a37d63f41088
Emmanuel Bello <emabello42@gmail.com>
2020-11-18 14:04:47 +0100

    feat: implements save image test case (working with mocks)

--- visualsearch/models/feature_extractor.py
+++ visualsearch/models/feature_extractor.py
@@ -4,8 +4,20 @@ from torchvision import datasets
 from torchvision import transforms
 import torch
 from PIL import Image as PILImage
-from collections import namedtuple
 import numpy as np
+from dataclasses import dataclass
+from visualsearch.configs import FeatureExtractorConfig as cfg
+from typing import List
+from collections import namedtuple
+
+@dataclass
+class ImageFeatures:
+    unit_features: np.ndarray = None
+    magnitude: float = None
+    score: float = None
+    label: int = None
+    path: str = None
+
 
 class FeatureExtractor():
 
@@ -25,41 +37,60 @@ class FeatureExtractor():
     def process_image(self, path):
         img = PILImage.open(path)
         data = self.data_transform(img).unsqueeze(0)
-        output_batch = self._compute_features(data)
+        output_batch = self.__compute_features(data)
 
-        features = {
-                'unit_features': output_batch.unit_features[0],
-                'magnitude': output_batch.magnitudes[0].item(),
-                'label': output_batch.category_ids[0],
-                'score': output_batch.scores[0]
-                }
+        img_features = ImageFeatures(
+                unit_features = output_batch.unit_features[0],
+                magnitude = output_batch.magnitudes[0].item(),
+                label = output_batch.labels[0],
+                score = output_batch.scores[0],
+                path = path
+                )
 
-        return features
+        return img_features
 
     def process_batch(self, path):
-        pass
+        image_data = datasets.ImageFolder(path, transform=self.data_transform)
+        image_loader = torch.utils.data.DataLoader(image_data, cfg.batch_size, shuffle=False,
+                                                   num_workers=cfg.num_workers)
 
-    def _compute_features(self, input_batch):
+        img_idx = 0
+        for batch_idx, (data, _) in enumerate(image_loader):
+            output_batch = self.__compute_features(data)
+            for unit_feat, mag, label, score in zip(output_batch.unit_features,
+                                                    output_batch.magnitudes,
+                                                    output_batch.labels,
+                                                    output_batch.scores):
+                img_features = ImageFeatures(unit_features = unit_feat,
+                                             magnitude = mag,
+                                             label = label,
+                                             score = score,
+                                             path = image_data.imgs[img_idx][0])
+                img_idx += 1
+                yield img_features
+
+    def __compute_features(self, input_batch):
         self.pstats.start("compute_features")
-        output_batch = namedtuple(
-            "BatchFeatures", "unit_features magnitudes scores category_ids")
+        output_batch = namedtuple("BatchFeatures",
+                                  "unit_features magnitudes scores labels")
         if self.use_gpu:
             input_batch = input_batch.to('cuda')
         with torch.no_grad():
             logits, unit_features, magnitudes = self.model(input_batch)
 
         probabilities = torch.nn.functional.softmax(logits, dim=1)
-        category_ids = torch.argmax(probabilities, dim=1)
+        labels = torch.argmax(probabilities, dim=1)
 
         probabilities = probabilities.cpu().numpy()
-        category_ids = category_ids.cpu().numpy()
+        labels = labels.cpu().numpy()
         scores = []
         for i, p in enumerate(probabilities):
-            scores.append(p[category_ids[i]])
+            scores.append(p[labels[i]])
 
         output_batch.unit_features = unit_features.cpu().numpy()
         output_batch.magnitudes = magnitudes.cpu().numpy()
         output_batch.scores = np.array(scores)
-        output_batch.category_ids = category_ids
+        output_batch.labels = labels
         self.pstats.end("compute_features")
         return output_batch
+
