86205d3ff1330dfba89a7c57d1f42bb60c82769c
Emmanuel Bello <emabello42@gmail.com>
2020-11-22 00:07:40 +0100

    test: refactor feature extractor test

--- tests/models/test_feature_extractor.py
+++ tests/models/test_feature_extractor.py
@@ -2,25 +2,28 @@ from visualsearch.models import feature_extractor as fe
 import os
 import pytest
 import numpy as np
-
+from visualsearch.domain import image as i
 FIXTURE_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../testdata")
+
 
 def test_process_image():
     feature_extractor = fe.FeatureExtractor()
     file_img = os.path.join(FIXTURE_DIR, "img1.jpg")
-    features = feature_extractor.process_image(path = file_img)
+    features = feature_extractor.process_image(path=file_img)
     assert np.linalg.norm(features.unit_features) == pytest.approx(1.0)
     assert features.magnitude >= 0
     assert features.path == file_img
 
+
 def test_process_image_batch():
     feature_extractor = fe.FeatureExtractor()
     file_list = [
-                os.path.join(FIXTURE_DIR, "img1.jpg"),
-                os.path.join(FIXTURE_DIR, "img2.jpg"),
-                os.path.join(FIXTURE_DIR, "img3.jpg")
-                ]
-    for img in feature_extractor.process_batch(path = FIXTURE_DIR):
-        assert np.linalg.norm(img.unit_features) == pytest.approx(1.0)
-        assert img.magnitude >= 0
-        assert img.path in file_list
+        os.path.join(FIXTURE_DIR, "img1.jpg"),
+        os.path.join(FIXTURE_DIR, "img2.jpg"),
+        os.path.join(FIXTURE_DIR, "img3.jpg")
+    ]
+    for output_batch, paths in feature_extractor.process_batch(path=FIXTURE_DIR):
+        for idx, (unit_features, magnitude) in enumerate(zip(output_batch.unit_features, output_batch.magnitudes)):
+            assert np.linalg.norm(unit_features) == pytest.approx(1.0)
+            assert magnitude >= 0
+            assert paths[idx] in file_list
