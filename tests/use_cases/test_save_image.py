0668ff043e5c8a37e05d8df176381a03ae3d15b9
Emmanuel Bello <emabello42@gmail.com>
2020-11-24 18:37:02 +0100

    refactor

--- tests/use_cases/test_save_image.py
+++ tests/use_cases/test_save_image.py
@@ -13,18 +13,19 @@ FIXTURE_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../test
 
 @pytest.fixture
 def domain_images():
-    img1 = Image(code=uuid.uuid4(), path = os.path.join(FIXTURE_DIR, "img1.jpg"),
-            unit_features = np.arange(2048),
-            magnitude = 100.1)
-    img2 = Image(code=uuid.uuid4(), path = os.path.join(FIXTURE_DIR, "img2.jpg"),
-            unit_features = np.arange(2048),
-            magnitude = 90.8)
+    img1 = Image(code=uuid.uuid4(), path=os.path.join(FIXTURE_DIR, "img1.jpg"),
+                 unit_features=np.arange(2048),
+                 magnitude=100.1)
+    img2 = Image(code=uuid.uuid4(), path=os.path.join(FIXTURE_DIR, "img2.jpg"),
+                 unit_features=np.arange(2048),
+                 magnitude=90.8)
 
-    img3 = Image(code=uuid.uuid4(), path = os.path.join(FIXTURE_DIR, "img3.jpg"),
-            unit_features = np.arange(2048),
-            magnitude = 91.8)
+    img3 = Image(code=uuid.uuid4(), path=os.path.join(FIXTURE_DIR, "img3.jpg"),
+                 unit_features=np.arange(2048),
+                 magnitude=91.8)
 
     return [img1, img2, img3]
+
 
 def test_save_image(domain_images):
     repo = mock.Mock()
@@ -32,22 +33,19 @@ def test_save_image(domain_images):
 
     file_path = domain_images[0].path
 
-    # fake image feature values
-    unit_features = np.arange(2048)
-    magnitude = 100
-
     # expected return values for mocked functions
     repo.save_image.return_value = 1
     feature_extractor.process_image.return_value = domain_images[0]
 
     use_case_save_image = uc.SaveImage(repo, feature_extractor)
-    request_object = req.ImageRequestObject.from_dict({'params':{'path': file_path}})
+    request_object = req.ImageRequestObject.from_dict({'params': {'path': file_path}})
     response = use_case_save_image.execute(request_object)
     logging.debug(response.value)
     assert bool(response) is True
     feature_extractor.process_image.assert_called_with(file_path)
     repo.save_image.assert_called_with(domain_images[0])
     assert response.value == 1
+
 
 def test_save_image_all(domain_images):
     repo = mock.Mock()
@@ -58,7 +56,7 @@ def test_save_image_all(domain_images):
     repo.close_save_batch_process.return_value = 3
     feature_extractor.process_batch.return_value = domain_images
 
-    request_object = req.ImageRequestObject.from_dict({'params':{'path': FIXTURE_DIR}})
+    request_object = req.ImageRequestObject.from_dict({'params': {'path': FIXTURE_DIR}})
     use_case_save_image = uc.SaveImage(repo, feature_extractor)
     response = use_case_save_image.execute(request_object)
     logging.debug(response.value)
